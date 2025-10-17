582c2630a54a6805de438144a5a9fdb1d896c8d4
swati sinha <swatisinha@gmail.com>
2025-10-17 13:37:43 +0530

    background changer

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+ import { BackgroundChange } from './component/Background color changer/BackgroundChange'
+const App = () => {
+  return (
+    <div>
+    <BackgroundChange/>
+    </div>
+  )
+}
+
+export default App
